830d1aec7811d9386afb298f05d28aec15aaf38a
0xCipherCoder <ciphercoder0x@gmail.com>
2024-09-10 10:44:01 +0530

    Updated code snippets and readme

--- programs/config/src/instructions/program_config/update_program_config.rs
+++ programs/config/src/instructions/program_config/update_program_config.rs
@@ -1,6 +1,5 @@
 use crate::state::ProgramConfig;
-use crate::SEED_PROGRAM_CONFIG;
-use crate::USDC_MINT_PUBKEY;
+use crate::{SEED_PROGRAM_CONFIG, USDC_MINT_PUBKEY};
 use anchor_lang::prelude::*;
 use anchor_spl::token::TokenAccount;
 
@@ -8,12 +7,9 @@ use anchor_spl::token::TokenAccount;
 pub struct UpdateProgramConfig<'info> {
     #[account(mut, seeds = [SEED_PROGRAM_CONFIG], bump)]
     pub program_config: Account<'info, ProgramConfig>,
-    #[account( token::mint = USDC_MINT_PUBKEY)]
+    #[account(token::mint = USDC_MINT_PUBKEY)]
     pub fee_destination: Account<'info, TokenAccount>,
-    #[account(
-        mut,
-        address = program_config.admin,
-    )]
+    #[account(mut, address = program_config.admin)]
     pub admin: Signer<'info>,
     /// CHECK: arbitrarily assigned by existing admin
     pub new_admin: UncheckedAccount<'info>,
@@ -23,8 +19,10 @@ pub fn update_program_config_handler(
     ctx: Context<UpdateProgramConfig>,
     new_fee: u64,
 ) -> Result<()> {
-    ctx.accounts.program_config.admin = ctx.accounts.new_admin.key();
-    ctx.accounts.program_config.fee_destination = ctx.accounts.fee_destination.key();
-    ctx.accounts.program_config.fee_basis_points = new_fee;
+    ctx.accounts.program_config.set_inner(ProgramConfig {
+        admin: ctx.accounts.new_admin.key(),
+        fee_destination: ctx.accounts.fee_destination.key(),
+        fee_basis_points: new_fee,
+    });
     Ok(())
 }
--- programs/config/src/state.rs
+++ programs/config/src/state.rs
@@ -1,12 +1,9 @@
 use anchor_lang::prelude::*;
 
 #[account]
+#[derive(InitSpace)]
 pub struct ProgramConfig {
     pub admin: Pubkey,
     pub fee_destination: Pubkey,
     pub fee_basis_points: u64,
-}
-
-impl ProgramConfig {
-    pub const LEN: usize = 8 + 32 + 32 + 8;
 }
